cf8ba2d2f32e073c0bbb91eebf5f31d771bad108
sahilgk-cms <155707816+sahilgk-cms@users.noreply.github.com>
2025-06-06 18:01:48 +0530

    added url for groupby sentiment

--- ph/urls.py
+++ ph/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.ArticleListAV.as_view(), name = "article-index-page"),
+    path("group_by_sentiment/", views.GroupBySentimentAV.as_view(), name = "group-by-sentiment"),
     path("<str:pk>/", views.ArticleDetailAV.as_view(), name = "article-detail-page")
+   
 ]
