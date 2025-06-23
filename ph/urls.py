25aa76e524ebec82191fc45c9ad3e69d87caf42f
sahilgk-cms <sahil@catalysts.org>
2025-06-23 10:14:03 +0530

    updated urls with reports views

--- ph/urls.py
+++ ph/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.ArticleListAV.as_view(), name = "article-index-page"),
     path("group_by_sentiment/", views.GroupBySentimentAV.as_view(), name = "group-by-sentiment"),
+    path("generate_summary/", views.GenerateSummaryView.as_view(), name = "summary-page"),
+    path("generate_situational_report/", views.GenerateSituationalReportView.as_view(), name = "situational-report-page"),
+    path("generate_risk_assessment/", views.GenerateRiskAssessmentReportView.as_view(), name = "risk-assessment-page"),
     path("<str:pk>/", views.ArticleDetailAV.as_view(), name = "article-detail-page")
    
 ]
